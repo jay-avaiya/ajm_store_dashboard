55364ffca5cc680450f06b2985fbb53e93a0de6a
jay-avaiya <jay@pruthatek.com>
2026-01-21 16:40:14 +0530

    Upd: App Page

--- src/pages/Main/AppIntegrationPage/AppIntegration.jsx
+++ src/pages/Main/AppIntegrationPage/AppIntegration.jsx
@@ -9,7 +9,7 @@ const AppIntegration = () => {
   return (
     <div className="flex-1 flex justify-between pt-8 min-h-0 h-full overflow-hidden">
       {/* Left content */}
-      <div className="relative w-full max-w-[980px] h-full min-h-0 overflow-y-auto overflow-x-hidden no-scrollbar no-scroll-chain pr-2 pb-4">
+      <div className="relative w-full max-w-[100%] h-full min-h-0 overflow-y-auto overflow-x-hidden no-scrollbar no-scroll-chain pr-2 pb-4">
         {/* Header content */}
         <div className="w-full flex items-center justify-between">
           <div className="flex items-center gap-x-3">
@@ -21,11 +21,11 @@ const AppIntegration = () => {
               <input
                 type="text"
                 placeholder="Search..."
-                className="border border-[#B0B0B0] placeholder:text-[#8E8E8E] outline-none py-3 px-3  rounded-full w-full bg-white"
+                className="border border-[#B0B0B0] placeholder:text-[#8E8E8E] outline-none p-2 rounded-full w-full bg-white"
               />
               <FaMagnifyingGlass className="size-5 absolute right-3 text-[#1D7BBB]" />
             </div>
-            <div className=" bg-white border border-[#B0B0B0] rounded-full flex items-center justify-center py-3 px-2">
+            <div className=" bg-white border border-[#B0B0B0] rounded-full flex items-center justify-center p-2">
               <span className="text-[#222222] text-[16px]">
                 4000 Listed Products
               </span>
@@ -37,27 +37,27 @@ const AppIntegration = () => {
         <div className="w-full h-[1px] border border-[#C0C0C0] my-4"></div>
 
         {/* stat card */}
-        <div className="w-full flex items-center justify-between">
+        <div className="w-full flex items-center gap-x-3">
           {/* 1st card - Total App Users */}
-          <div className="bg-[#F9FDFF] rounded-3xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] text-[#34C759] text-xl  p-2 flex items-center justify-between w-[230px] h-[74px]">
+          <div className="bg-[#F9FDFF] rounded-3xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] text-[#34C759] text-xl  p-2 flex items-center justify-between w-[25%] h-[74px]">
             <h4 className="w-[60%]">Total App Users</h4>
             <p>18,420</p>
           </div>
 
           {/* 2nd card - Active users */}
-          <div className="bg-[#F9FDFF] rounded-3xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] text-[#1D7BBB] text-xl  p-2 flex items-center justify-between w-[230px] h-[74px]">
+          <div className="bg-[#F9FDFF] rounded-3xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] text-[#1D7BBB] text-xl  p-2 flex items-center justify-between w-[25%] h-[74px]">
             <h4 className="w-[60%]">Active Users (Today)</h4>
             <p>18,420</p>
           </div>
 
           {/* 3rd card - App Installs  */}
-          <div className="bg-[#F9FDFF] rounded-3xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] text-[#8A38F5] text-xl  p-2 flex items-center justify-between w-[230px] h-[74px]">
+          <div className="bg-[#F9FDFF] rounded-3xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] text-[#8A38F5] text-xl  p-2 flex items-center justify-between w-[25%] h-[74px]">
             <h4 className="w-[40%]">App Installs</h4>
             <p>27,940</p>
           </div>
 
           {/*  4th card - New Users */}
-          <div className="bg-[#F9FDFF] rounded-3xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] text-[#FF7049] text-xl  p-2 flex items-center justify-between w-[230px] h-[74px]">
+          <div className="bg-[#F9FDFF] rounded-3xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] text-[#FF7049] text-xl  p-2 flex items-center justify-between w-[25%] h-[74px]">
             <h4 className="">New Users</h4>
             <p>986</p>
           </div>
@@ -68,7 +68,7 @@ const AppIntegration = () => {
       </div>
 
       {/* Right content */}
-      <div className="flex flex-col px-4 gap-y-4 mb-4 h-full min-h-0 max-w-[320px] shrink-0 overflow-y-auto overflow-x-hidden no-scrollbar no-scroll-chain mr-2 pb-4">
+      <div className="flex flex-col px-4 gap-y-4 mb-4 h-full min-h-0 max-w-[26%] w-full shrink-0 overflow-y-auto overflow-x-hidden no-scrollbar no-scroll-chain mr-2 pb-4">
         {/* Notifications */}
         <NotificationBox />
         <FilterBox />
